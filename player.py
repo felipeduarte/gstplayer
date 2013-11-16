e76f189d8feec6310492238f792b30cac95e45d2
Felipe Duarte <felipe.duarte55@gmail.com>
2013-11-16 02:39:13 -0200

    Corriji alguns problemas de identação no método tocar, adicionei comentários, dei nome mais lógico a alguns métodos

--- player.py
+++ player.py
@@ -1,15 +1,17 @@
 #coding: utf-8
-import pygtk
+
 import gtk
+import pygtk
 pygtk.require('2.0')
+
 import pygst
 pygst.require('0.10')
 import gst
 
-
 class Tela(object):    
 
     def __init__(self):
+        #janela do aplicativo
         win = gtk.Window()
         win.set_title("GstPlayer")
         win.set_size_request(500, 300)
@@ -23,25 +25,25 @@ class Tela(object):
         fixar.show()
 
         #botão play:
-        bt_play = gtk.Button("Play")
-        fixar.put(bt_play, 50, 100)
-        container.pack_start(bt_play)
-        bt_play.connect("clicked", self.clicada_play)
+        botao_play = gtk.Button("Play")
+        fixar.put(botao_play, 50, 100)
+        container.pack_start(botao_play)
+        botao_play.connect("clicked", self.tocar)
 
         #botão stop:
-        bt_stop = gtk.Button("Stop")
-        fixar.put(bt_stop, 100, 100)
-        container.pack_start(bt_stop)
-        bt_stop.connect("clicked", self.clicada_stop)
+        botao_stop = gtk.Button("Stop")
+        fixar.put(botao_stop, 100, 100)
+        container.pack_start(botao_stop)
+        botao_stop.connect("clicked", self.parar)
 
         #botão pause:
-        bt_pause = gtk.Button("Pause")
-        fixar.put(bt_pause, 150, 100)
-        container.pack_start(bt_pause)
-        bt_pause.connect("clicked", self.clicada_pause)
+        botao_pause = gtk.Button("Pause")
+        fixar.put(botao_pause, 150, 100)
+        container.pack_start(botao_pause)
+        botao_pause.connect("clicked", self.pausar)
 
         win.show_all()
-
+        #montando a paipleline p/ tocar a mp3
         self.paipe = gst.Pipeline('paipeline')
         self.source = gst.element_factory_make('filesrc', 'source')
         self.decoder = gst.element_factory_make('mad', 'decoder')
@@ -55,17 +57,18 @@ class Tela(object):
         self.paipe.add(self.resample)
         self.paipe.add(self.sink)
 
-        gst.element_link_many(self.source, self.decoder, self.convert, self.resample, self.sink)
+        gst.element_link_many(self.source, self.decoder, self.convert, 
+                                self.resample, self.sink)
 
         self.paipe.set_state(gst.STATE_READY)
 
-    def clicada_play(self, bt_play):
-        if self.paipe.get_state()[1] == gst.STATE_READY or self.paipe.get_state()[1] == gst.STATE_NULL:
+    def tocar(self, botao_play):
+        if (self.paipe.get_state()[1] == gst.STATE_READY or 
+                self.paipe.get_state()[1] == gst.STATE_NULL):
             dialog = gtk.FileChooserDialog("Open..",
-                                   None,
-                                   gtk.FILE_CHOOSER_ACTION_OPEN,
-                                   (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
-                                    gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+                None, gtk.FILE_CHOOSER_ACTION_OPEN,
+                    (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, 
+                    gtk.RESPONSE_OK))
             dialog.set_default_response(gtk.RESPONSE_OK)
             filter = gtk.FileFilter()
             filter.set_name("All files")
@@ -90,22 +93,22 @@ class Tela(object):
             self.confirmar_tocando()
     
     def confirmar_tocando(self):
-        self.aviso = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        self.aviso.set_border_width(10)
-        self.aviso.set_title("Aviso")
-        self.mensagem = gtk.Label("A música já esta em execução!!")
+        aviso = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        aviso.set_border_width(10)
+        aviso.set_title("Aviso")
+        mensagem = gtk.Label("A música já esta em execução!!")
         container = gtk.HBox(False, 0)
-        self.aviso.add(container)
+        aviso.add(container)
         fixar = gtk.Fixed()
         container.pack_start(fixar)
         fixar.show()
-        container.pack_start(self.mensagem)
-        self.aviso.show_all()
+        container.pack_start(mensagem)
+        aviso.show_all()
 
-    def clicada_stop(self, bt_stop):
+    def parar(self, botao_stop):
         self.paipe.set_state(gst.STATE_NULL)
 
-    def clicada_pause(self, bt_pause):
+    def pausar(self, botao_pause):
         if self.paipe.get_state()[1] == gst.STATE_PLAYING:
             self.paipe.set_state(gst.STATE_PAUSED)
 
